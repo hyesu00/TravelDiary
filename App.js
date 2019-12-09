88b48bbfbfc3452149d1bbd5be22aeded8b4a4ee
hyesu00 <qjqmfrja00@gmail.com>
2019-12-09 13:36:09 +0900

    프로젝트 생성

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<LoginScreen />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+	flex: 1,
+	backgroundColor: '#fff',
+	alignItems: 'center',
+	justifyContent: 'center',
+	},
+});
