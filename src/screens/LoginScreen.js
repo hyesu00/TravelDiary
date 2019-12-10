11396e17fb0e43c8526d120e687d53e21c77bc26
saeyeong <syeongppp@gmail.com>
2019-12-10 15:49:52 +0900

    컴포넌트추가

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,27 +1,26 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {TouchableOpacity, View, Text, Image, TextInput} from 'react-native';
+import Button from './../components/Button';
 
 export default class LoginScreen extends Component {
 	render() {
 		return(
-			<View style={styles.container}>
-				<Text>LoginScreen</Text>
-				<TouchableOpacity onPress={() => this.props.navigation.navigate('AgreementScreen')}>
-					<Text>회원가입</Text>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={() => this.props.navigation.navigate('MainScreen')}>
-					<Text>로그인</Text>
-				</TouchableOpacity>
+			<View style={{flex: 1, alignItems: 'center'}}>
+				<Image style={{marginTop: 60, marginBottom: 20}} source={require('../images/logo.png')}/>
+				<View style={{borderRadius: 5, width: 300, height: 40, borderColor: 'gray', borderWidth: 1, overflow:"hidden" }}><TextInput placeholder="Enter Your ID"/></View>
+				<View style={{marginTop: 10, marginBottom: 15, borderRadius: 5, width: 300, height: 40, borderColor: 'gray', borderWidth: 1, overflow:"hidden" }}><TextInput placeholder="Enter Your PW"/></View>
+
+				<Button width={300} height={43} textColor={'white'} title={'로그인'} />
+				
+				<View style={{flex: 1, flexDirection: 'row', marginTop: 40}}>
+					<TouchableOpacity style={{width: 100}} onPress={() => this.props.navigation.navigate('AgreementScreen')}>
+						<Text style={{color: '#909090', textAlign: 'center'}}>회원가입</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={{width: 100}}>
+						<Text style={{color: '#909090', textAlign: 'center'}}>id/pw 찾기</Text>
+					</TouchableOpacity>
+				</View>
 			</View>
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
-
