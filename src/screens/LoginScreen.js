c989daf5ec3efdb1c370c6bfa1f6ecf8dfd723a4
hyesu00 <qjqmfrja00@gmail.com>
2019-12-17 13:15:58 +0900

    구글 로그인 API 추가

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,8 +1,78 @@
 import React, {Component} from 'react';
 import {TouchableOpacity, View, Text, Image, TextInput} from 'react-native';
+import * as GoogleSignIn from 'expo-google-sign-in';
 import Button from './../components/Button';
 
 export default class LoginScreen extends Component {
+	state = { user: null };
+
+	componentDidMount() {
+		this.initAsync();
+	}
+
+	initAsync = async () => {
+		await GoogleSignIn.initAsync({
+		clientId: '<YOUR_IOS_CLIENT_ID>',
+		});
+		this._syncUserWithStateAsync();
+	};
+
+	_syncUserWithStateAsync = async () => {
+		const user = await GoogleSignIn.signInSilentlyAsync();
+		this.setState({ user });
+	};
+
+	signOutAsync = async () => {
+		await GoogleSignIn.signOutAsync();
+		this.setState({ user: null });
+	};
+
+	signInAsync = async () => {
+		try {
+		await GoogleSignIn.askForPlayServicesAsync();
+		const { type, user } = await GoogleSignIn.signInAsync();
+		if (type === 'success') {
+			this._syncUserWithStateAsync();
+			alert('login: successs2:');
+		}
+		} catch ({ message }) {
+		alert('login: Error:' + message);
+		}
+	};
+
+	/* 로그인, 로그아웃 
+	state = {user: null};
+
+	signInAsync = async () => {
+		try {
+			await GoogleSignIn.askForPlayServicesAsync();
+			const { type, user } = await GoogleSignIn.signInAsync();
+			if (type === 'success') {
+			// ...
+			alert('login: successs1:');
+			}
+		} catch ({ message }) {
+			alert('login: Error:' + message);
+		}
+	};
+
+	signOutAsync = async () => {
+		try {
+			await GoogleSignIn.signOutAsync();
+			this.setState({ user: null });
+		} catch ({ message }) {
+			alert('signOutAsync: ' + message);
+		}
+	}; */
+
+	onPress = () => {
+		if (this.state.user) {
+			this.signOutAsync();
+		} else {
+			this.signInAsync();
+		}
+	};
+	
 	render() {
 		return(
 			<View style={{flex: 1, alignItems: 'center'}}>
@@ -10,8 +80,10 @@ export default class LoginScreen extends Component {
 				<View style={{borderRadius: 5, width: 300, height: 40, borderColor: 'gray', borderWidth: 1, overflow:"hidden" }}><TextInput style={{paddingTop:5, paddingLeft:10}} placeholder="Enter Your ID"/></View>
 				<View style={{marginTop: 10, marginBottom: 15, borderRadius: 5, width: 300, height: 40, borderColor: 'gray', borderWidth: 1, overflow:"hidden" }}><TextInput style={{paddingTop:5,paddingLeft:10}} placeholder="Enter Your PW"/></View>
 
-				<Button width={300} height={43} textColor={'white'} title={'로그인'} />
-				
+				<Button width={300} height={43} textColor={'white'} title={'로그인!'} />
+
+				<Text onPress={this.onPress}>google Login</Text>
+
 				<View style={{flex: 1, flexDirection: 'row', marginTop: 40}}>
 					<TouchableOpacity style={{width: 100, height: 40}} onPress={() => this.props.navigation.navigate('AgreementScreen')}>
 						<Text style={{color: '#909090', textAlign: 'center'}}>회원가입</Text>
